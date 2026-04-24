aa13db89c7614e66d7e27184094903e688fbc994
SvalPercy <a20192356@pucp.edu.pe>
2026-04-24 16:44:38 -0500

    Create productos.js

---
+++ productos.js
@@ -0,0 +1,11 @@
+const productos = [
+  {
+    id: "099160009",
+    nombre: "DP A HDMI 4K UGREEN COD 55935 PACK DE 2",
+    precio: 50,
+    marca: "UGREEN",
+    stock: 2,
+    imagenes: ["default.webp"],
+    descripcion: ["Producto UGREEN"]
+  }
+];
